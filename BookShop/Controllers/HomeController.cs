53708c6ad5d046a58b6b77c21dbaec1776e86558
cuongnm2002 <nguyenmanhcuong02062002@gmail.com>
2025-04-24 22:54:58 +0700

    bookshop

---
+++ BookShop/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ShopContext db = new ShopContext();
+
+        //Trang chủ admin
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+
+        //Menu con
+        [ChildActionOnly]
+        public PartialViewResult SubMenu()
+        {
+            var query = db.tb_Category.ToList();
+            return PartialView(query);
+        }
+        public ActionResult Index()
+        {
+            var moive1 = db.tb_Product.OrderByDescending(x => x.id).Take(4).ToList();
+            var moive2 = db.tb_Product.OrderByDescending(x => x.id).Skip(4).Take(4).ToList();
+
+            ViewBag.moive1 = moive1;
+            ViewBag.moive2 = moive2;
+
+            var hot1 = db.tb_Product.OrderByDescending(x => x.price_new).Take(4).ToList();
+            var hot2 = db.tb_Product.OrderByDescending(x => x.price_new).Skip(4).Take(4).ToList();
+
+            ViewBag.hot1 = hot1;
+            ViewBag.hot2 = hot2;
+
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BookShop/Models/ShopContext.cs
@@ -0,0 +1,81 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BookShop.Models
+{
+    public partial class ShopContext : DbContext
+    {
+        public ShopContext()
+            : base("name=ShopContext")
+        {
+        }
+
+        public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
+        public virtual DbSet<tb_Brand> tb_Brand { get; set; }
+        public virtual DbSet<tb_Category> tb_Category { get; set; }
+        public virtual DbSet<tb_Order> tb_Order { get; set; }
+        public virtual DbSet<tb_OrderDetail> tb_OrderDetail { get; set; }
+        public virtual DbSet<tb_Product> tb_Product { get; set; }
+        public virtual DbSet<tb_Role> tb_Role { get; set; }
+        public virtual DbSet<tb_User> tb_User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<tb_Brand>()
+                .Property(e => e.logo)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tb_Brand>()
+                .HasMany(e => e.tb_Product)
+                .WithOptional(e => e.tb_Brand)
+                .HasForeignKey(e => e.brand_id);
+
+            modelBuilder.Entity<tb_Category>()
+                .HasMany(e => e.tb_Product)
+                .WithOptional(e => e.tb_Category)
+                .HasForeignKey(e => e.category_id);
+
+            modelBuilder.Entity<tb_Order>()
+                .Property(e => e.sdt)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tb_Order>()
+                .HasMany(e => e.tb_OrderDetail)
+                .WithOptional(e => e.tb_Order)
+                .HasForeignKey(e => e.order_id);
+
+            modelBuilder.Entity<tb_Product>()
+                .Property(e => e.size)
+                .IsFixedLength();
+
+            modelBuilder.Entity<tb_Product>()
+                .HasMany(e => e.tb_OrderDetail)
+                .WithOptional(e => e.tb_Product)
+                .HasForeignKey(e => e.product_id);
+
+            modelBuilder.Entity<tb_Role>()
+                .HasMany(e => e.tb_User)
+                .WithOptional(e => e.tb_Role)
+                .HasForeignKey(e => e.role_id);
+
+            modelBuilder.Entity<tb_User>()
+                .Property(e => e.username)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tb_User>()
+                .Property(e => e.phonenumber)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tb_User>()
+                .Property(e => e.passwd)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<tb_User>()
+                .HasMany(e => e.tb_Order)
+                .WithOptional(e => e.tb_User)
+                .HasForeignKey(e => e.userid);
+        }
+    }
+}
